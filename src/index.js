8910a68e8968155cf134fe3e47f2298ab63a58bd
Ctharp11 <ctharp1988@gmail.com>
2018-10-22 10:54:05 -0500

    webpack with sass

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import './app.css';
+import './styles/styles.scss';
 
 const Index = () => {
     return <div className="index">Hello React working with weasasdadbpack</div>;
