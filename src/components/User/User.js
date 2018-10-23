a769b23bfcd5dd1559a03c5de9aac894a5c2c993
Ctharp11 <ctharp1988@gmail.com>
2018-10-23 14:20:13 -0500

    graph added

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -4,7 +4,7 @@ import UserChart from './UserChart';
 const User = (props) => (
     <div className="user container"> 
       <div className="user-head"> Welcome, {props.userInfo.name}! </div>
-      <div className="user-text"> Here's your energy usage for the year. </div>
+      <div className="user-text"> Here's your energy usage for the year. All numbers measured in kilowatthours (kWh). </div>
       <UserChart usage={props.userInfo.usage}  />
     </div>
 )
--- src/components/User/UserChart.js
+++ src/components/User/UserChart.js
@@ -6,7 +6,7 @@ import Chart from 'chart.js'
 const UserChart = (props) => (
   <div>
   {console.log(props.usage)}
-  <LineChart data={props.usage} />
+  <LineChart data={props.usage} min={900} max={1000} width="95%" height="400px" />
   
   </div>
 );
